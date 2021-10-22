f052ce2330ab168fc69923ad529784025ac1696f
Pratikxya <pratikshyapoudel2000@gmail.com>
2021-10-22 13:25:12 +0545

    Added socket disconnection handler

--- server.js
+++ server.js
@@ -12,12 +12,16 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //Run when client connects
 io.on("connection", (socket) => {
-  console.log("New WS Connection...");
-
-  socket.emit("message", "A user has joined the chat"); //to single client
+  //Welcome current user
+  socket.emit("message", "Welcome to chatcord !"); //to single client
 
   //Broadcast when a user connects
-  socket.broadcast.emit(); //to all clients except the one who is connecting
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
