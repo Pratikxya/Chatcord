74d4c80da0143f1dc2ccf8c14781c783e8b6a4d0
Pratikxya <pratikshyapoudel2000@gmail.com>
2021-10-22 13:57:49 +0545

    Set output message to DOM

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     io.emit("message", "A user has left the chat");
   });
+
+  //Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    io.emit;
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
